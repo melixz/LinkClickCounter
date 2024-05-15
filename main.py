01d63cc1f18f8fb95583f3e9ae16b39f34765688
Vlad <dr.melix@gmail.com>
2024-05-16 01:41:37 +0300

    Проверка ссылки пользователя(Ключ error в ответе от API VK)

--- main.py
+++ main.py
@@ -4,24 +4,34 @@ import requests
 def shorten_link(token, original_url):
     url = 'https://api.vk.com/method/utils.getShortLink'
     params = {
-        'v': '5.131',  # Версия API
-        'url': original_url,  # URL для сокращения
-        'access_token': token  # Токен доступа
+        'v': '5.131',
+        'url': original_url,
+        'access_token': token
     }
 
-    response = requests.get(url, params=params)
-    response.raise_for_status()  # Проверка на ошибки в запросе
+    try:
+        response = requests.get(url, params=params)
+        response.raise_for_status()  # Проверка стандартных HTTP ошибок
+        data = response.json()
 
-    data = response.json()
+        if 'error' in data:
+            error_msg = data['error']['error_msg']
+            raise Exception(f"Ошибка API: {error_msg}")
 
-    if 'response' in data:
-        print("Сокращенная ссылка:", data['response']['short_url'])
-    else:
-        print("Ошибка при создании сокращенной ссылки:", data['error']['error_msg'])
+        return data['response']['short_url']  # Возврат короткой ссылки
+    except requests.RequestException as e:
+        # Обработка исключений, связанных с сетью или HTTP ответами
+        raise Exception(f"Сетевая ошибка: {str(e)}")
+    except Exception as e:
+        # Обработка других видов ошибок, включая ошибки API
+        raise Exception(f"Ошибка при сокращении ссылки: {str(e)}")
 
 
-# Использование функции
 if __name__ == "__main__":
     token = 'd6621232d6621232d662123291d57a5c0edd662d6621232b026f18d5f6848561516b2cc'
     original_url = input("Введите URL для сокращения: ")
-    shorten_link(token, original_url)
+    try:
+        short_link = shorten_link(token, original_url)
+        print("Сокращенная ссылка:", short_link)
+    except Exception as e:
+        print(str(e))
